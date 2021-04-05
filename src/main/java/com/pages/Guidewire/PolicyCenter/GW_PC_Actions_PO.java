98b1e0444fd7257215dbb92ef1864a5df436333e
Akash <agowda@xlerateit.com>
2021-04-05 13:35:02 +0530

    Actions summary po added

---
+++ src/main/java/com/pages/Guidewire/PolicyCenter/GW_PC_Actions_PO.java
@@ -0,0 +1,32 @@
+package com.pages.Guidewire.PolicyCenter;
+
+public interface GW_PC_Actions_PO {
+// this is for Actions screen
+	
+	  //add xpath for New Note section
+		
+	  //add xpath for New Document section
+	 //add xpath for upload documents section
+	 //add xpath for create from a template section
+	 //add xpath for New Document section
+	
+     //add xpath for New Email Submissions section
+		
+	 //add xpath for New Submissions section
+	
+	//add xpath for New Activity section
+	//add xpath for Interviewer section
+	//add xpath for New mail section
+	//add xpath for Remainder section
+	//add xpath for Request section
+	
+	 //add xpath for Move Policies to this Account section
+	 //add xpath Rewrite Policies to this Account section
+	 //add xpath Merge Account into this Account  section
+	void newNote();
+	void newDocument();
+	
+	
+	
+
+}
---
+++ src/main/java/com/pages/Guidewire/PolicyCenter/GW_PC_MySummary.java
@@ -0,0 +1,49 @@
+package com.pages.Guidewire.PolicyCenter;
+
+import org.openqa.selenium.WebDriver;
+
+import com.Utils.Selenium.SeleniumWebDriver_Commands;
+import com.aventstack.extentreports.ExtentTest;
+
+public class GW_PC_MySummary extends SeleniumWebDriver_Commands implements GW_PC_MySummary_PO{
+	
+	public GW_PC_MySummary(WebDriver driver, ExtentTest oExtentTest) {
+		super(driver, oExtentTest);
+	}
+	@Override
+	public void overview() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void myActivities() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mySubmissions() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void myRenewals() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void otherPolicyTransactions() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void myQueues() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
