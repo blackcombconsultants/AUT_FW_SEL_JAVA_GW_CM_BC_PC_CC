489fb897c43db40a4343bc112602464cde62ad37
gkaranam <gkaranam@WSAMZN-5DOC9RM1.india-ad.blackcombcloud.com>
2021-04-06 15:38:54 +0530

    Updated changes in program and policycenter reusables. Changed gwAutomate to GuidewireAutomate and refining the program file to execute the scenario

--- src/test/java/com/Test/Guidewire/PolicyCenter/GW_PC_Program.java
+++ src/test/java/com/Test/Guidewire/PolicyCenter/GW_PC_Program.java
@@ -10,17 +10,18 @@ import com.pages.Guidewire.PolicyCenter.GW_PolicyCenter_Resuables;
 public class GW_PC_Program extends GW_GetDriver{
 	
 	@Test
-	public void BasicProgram() throws Exception {
-		GW_LoginCM_PC_BC_CC lg = new GW_LoginCM_PC_BC_CC(driver, oExtentTest);
-		GW_PolicyCenter_Resuables AccSer = new GW_PolicyCenter_Resuables(driver, oExtentTest);
-		GW_PC_AccountSummary newSub = new GW_PC_AccountSummary(driver, oExtentTest);
-		
-		
-		lg.login_PolicyCenter();
-		AccSer.Account_Search_SubTab();
-		newSub.AccountSummary_Actions_NewSubmission();
-		
+	public void NewBusiness() throws Exception {
+		try {
+			GW_LoginCM_PC_BC_CC lg = new GW_LoginCM_PC_BC_CC(driver, oExtentTest);
+			GW_PolicyCenter_Resuables AccSer = new GW_PolicyCenter_Resuables(driver, oExtentTest);
+			GW_PC_AccountSummary newSub = new GW_PC_AccountSummary(driver, oExtentTest);
+			
+			
+			lg.login_PolicyCenter();
+			AccSer.Account_Search_SubTab();
+			newSub.AccountSummary_Actions_NewSubmission();	
+		}catch (Exception e) {
+			e.printStackTrace();
+			}
 	}
-	
-
 }
