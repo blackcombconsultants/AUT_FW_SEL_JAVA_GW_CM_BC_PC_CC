e3d91d8b4a604458b9bb9aa045ea9ac89b245d4b
Akash <agowda@xlerateit.com>
2021-04-19 19:07:07 +0530

    coverages and quote and forms updated

--- src/test/java/com/Test/Guidewire/PolicyCenter/GW_PC_PersonalAuto_Coverages.java
+++ src/test/java/com/Test/Guidewire/PolicyCenter/GW_PC_PersonalAuto_Coverages.java
@@ -1,21 +1,16 @@
 package com.Test.Guidewire.PolicyCenter;
 
-import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
 import com.Utils.Selenium.GW_GetDriver;
-import com.aventstack.extentreports.ExtentTest;
 import com.pages.Guidewire.PersonalAuto.PersonalAuto_Coverages;
 
-public class GW_PC_PersonalAuto_Coverages extends GW_GetDriver{
+public class GW_PC_PersonalAuto_Coverages extends GW_GetDriver {
 
 	@Test
-	public void AUT_PC_AC_05() throws Throwable
-	{
-		PersonalAuto_Coverages pc=new PersonalAuto_Coverages(driver, oExtentTest);
-		//---->adding coverages applied to all vehicles
-		pc.pac_CoveragesAppliedtoallvehicles();
-		//---->searching AddExclusionorCondition
-		pc.pac_SearchAddExclusionorCondition();
+	public void AUT_PC_AC_05() throws Throwable {
+		PersonalAuto_Coverages pc = new PersonalAuto_Coverages(driver, oExtentTest);
+		// ---->adding coverages applied to all vehicles
+
 	}
 }
