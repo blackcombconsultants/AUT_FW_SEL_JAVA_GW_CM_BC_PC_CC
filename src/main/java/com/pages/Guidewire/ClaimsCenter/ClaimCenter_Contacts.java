b3d627a12a39a56d246a16102c879ed7f5481a72
labor <labor@JMD>
2021-06-03 18:40:21 +0530

    format

--- src/main/java/com/pages/Guidewire/ClaimsCenter/ClaimCenter_Contacts.java
+++ src/main/java/com/pages/Guidewire/ClaimsCenter/ClaimCenter_Contacts.java
@@ -13,27 +13,25 @@ public class ClaimCenter_Contacts extends SeleniumWebDriver_Commands implements 
 		super(driver, oExtentTest);
 		// TODO Auto-generated constructor stub
 	}
-	
-	public static void New_Contact() throws Throwable
-	{
-		lhm_TestCase_Table_Data = Selenium_Utils_DataBase.getData_MSExcel_WorkSheet_Fillo("contacts",
-				strTestCaseName);
-		GuidewireAutomate_Validation("Screen Header",Contacts_Header, "equals", "Contacts");
+
+	public static void New_Contact() throws Throwable {
+		lhm_TestCase_Table_Data = Selenium_Utils_DataBase.getData_MSExcel_WorkSheet_Fillo("contacts", strTestCaseName);
+		GuidewireAutomate_Validation("Screen Header", Contacts_Header, "equals", "Contacts");
 		GuidewireAutomate("NewContact", NewContact, "click", "NA");
 
-		String strDynamicxpath=lhm_TestCase_Table_Data.get("NewContact");
-		
-		final By NewContact_Person=By.xpath("//div[contains(@id,'ClaimContacts_CreateNewContactButton-ClaimContacts')]//div[@class='gw-label' and text()='"+strDynamicxpath+"']");
-		final By NewContact_Vendor=By.xpath("//div[contains(@id,'ClaimContacts_CreateNewContactButton-ClaimContacts')]//div[@class='gw-label' and text()='"+strDynamicxpath+"']");
-		final By NewContact_Company=By.xpath("//div[contains(@id,'ClaimContacts_CreateNewContactButton-ClaimContacts')]//div[@class='gw-label' and text()='"+strDynamicxpath+"']");
-		final By NewContact_Legal=By.xpath("//div[contains(@id,'ClaimContacts_CreateNewContactButton-ClaimContacts')]//div[@class='gw-label' and text()='"+strDynamicxpath+"']");
+		String strDynamicxpath = lhm_TestCase_Table_Data.get("NewContact");
+
+		final By NewContact_Person  = By.xpath("//div[contains(@id,'ClaimContacts_CreateNewContactButton-ClaimContacts')]//div[@class='gw-label' and text()='" + strDynamicxpath + "']");
+		final By NewContact_Vendor  = By.xpath("//div[contains(@id,'ClaimContacts_CreateNewContactButton-ClaimContacts')]//div[@class='gw-label' and text()='" + strDynamicxpath + "']");
+		final By NewContact_Company = By.xpath("//div[contains(@id,'ClaimContacts_CreateNewContactButton-ClaimContacts')]//div[@class='gw-label' and text()='" + strDynamicxpath + "']");
+		final By NewContact_Legal   = By.xpath("//div[contains(@id,'ClaimContacts_CreateNewContactButton-ClaimContacts')]//div[@class='gw-label' and text()='" + strDynamicxpath + "']");
 		GuidewireAutomate("NewContact Person", NewContact_Vendor, "moveToElement", "NA");
 		GuidewireAutomate("Autobody Repair Shop", NC_V_AutobodyRepairShop, "click", "NA");
-		GuidewireAutomate_Validation("Screen Header",NC_V_ARS_Header, "equals", "New Auto Repair Shop");
+		GuidewireAutomate_Validation("Screen Header", NC_V_ARS_Header, "equals", "New Auto Repair Shop");
 		GuidewireAutomate("Add", NC_V_Add, "click", "NA");
 		GuidewireAutomate("Role", NC_V_Role, "selectByVisibleText", lhm_TestCase_Table_Data.get("Role"));
 		GuidewireAutomate("Comments", NC_V_Comments, "clearANDsendKeys", lhm_TestCase_Table_Data.get("Comments"));
-		
+
 		String strFirstName = lhm_TestCase_Table_Data.get("Name");
 		if (strFirstName.equalsIgnoreCase("Vendor Auto")) {
 			strFirstName = getRandomAlphabetic(9);
@@ -41,26 +39,22 @@ public class ClaimCenter_Contacts extends SeleniumWebDriver_Commands implements 
 		GuidewireAutomate("Name", NC_V_ARS_Name, "clearANDsendKeys", strFirstName);
 		GuidewireAutomate("Address1", NC_V_PA_Address1, "clearANDsendKeys", lhm_TestCase_Table_Data.get("Address1"));
 		GuidewireAutomate("State", NC_V_PA_State, "selectByVisibleText", lhm_TestCase_Table_Data.get("State"));
-	      
+
 		String TaxID = lhm_TestCase_Table_Data.get("TaxID");
-	    if(TaxID.equalsIgnoreCase("345-34-349"))
-	    {
-		String NewTaxId=TaxID+getRandomNumeric(1);
-		GuidewireAutomate("Comments", NC_V_TI_TaxID, "clearANDsendKeys", NewTaxId);
-      }
-	    else
-	    {
-		GuidewireAutomate("Comments", NC_V_TI_TaxID, "clearANDsendKeys", TaxID);
-	    }
+		if (TaxID.equalsIgnoreCase("345-34-349")) {
+			String NewTaxId = TaxID + getRandomNumeric(1);
+			GuidewireAutomate("Comments", NC_V_TI_TaxID, "clearANDsendKeys", NewTaxId);
+		} else {
+			GuidewireAutomate("Comments", NC_V_TI_TaxID, "clearANDsendKeys", TaxID);
+		}
 
 		GuidewireAutomate("Update", NC_V_Update, "click", "NA");
-}
-	
-	public static void NewContact_DuplicateContactFound_Verify() throws Throwable
-	{
-		GuidewireAutomate_Validation("Screen Header",DuplicateContactFound_Header, "equals", "Duplicate Contact Found");
-		GuidewireAutomate_Validation("Error Message",DuplicateContactFound_ErrorMessage, "contains", "Contact Manager contains entries that matches your new contact");
+	}
 
-}
+	public static void NewContact_DuplicateContactFound_Verify() throws Throwable {
+		GuidewireAutomate_Validation("Screen Header", DuplicateContactFound_Header, "equals", "Duplicate Contact Found");
+		GuidewireAutomate_Validation("Error Message", DuplicateContactFound_ErrorMessage, "contains", "Contact Manager contains entries that matches your new contact");
+
+	}
 
 }
--- src/main/java/com/pages/Guidewire/ClaimsCenter/ClaimCenter_CreateRecovery.java
+++ src/main/java/com/pages/Guidewire/ClaimsCenter/ClaimCenter_CreateRecovery.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebDriver;
 import com.Utils.Selenium.SeleniumWebDriver_Commands;
 import com.Utils.Selenium.Selenium_Utils_DataBase;
 import com.aventstack.extentreports.ExtentTest;
-import com.pages.Guidewire.PolicyCenter.PolicyCenter_Resuables;
 
 public class ClaimCenter_CreateRecovery extends SeleniumWebDriver_Commands implements ClaimCenter_CreateRecovery_PO {
 
@@ -15,18 +14,15 @@ public class ClaimCenter_CreateRecovery extends SeleniumWebDriver_Commands imple
 	}
 
 	public static void createRecovery() throws Throwable {
-		String insuredName =ClaimCenter_Resuables.infoBar("InsuredName");
+		String insuredName = ClaimCenter_Resuables.infoBar("InsuredName");
 		lhm_TestCase_Table_Data = Selenium_Utils_DataBase.getData_MSExcel_WorkSheet_Fillo("recovery", strTestCaseName);
 		GuidewireAutomate_Validation("Screen Header", CreateRecovery_Header, "equals", "Create Recovery");
 		GuidewireAutomate("Payer", CR_Payer, "selectByVisibleText", insuredName);
-		GuidewireAutomate("Reserve Line", CR_ReserveLine, "selectByVisibleText",
-				lhm_TestCase_Table_Data.get("ReserveLine"));
+		GuidewireAutomate("Reserve Line", CR_ReserveLine, "selectByVisibleText", lhm_TestCase_Table_Data.get("ReserveLine"));
 		GuidewireAutomate("CostType", PD_CostType, "selectByVisibleText", lhm_TestCase_Table_Data.get("CostType"));
-		GuidewireAutomate("CostCategory", PD_CostCategory, "selectByVisibleText",
-				lhm_TestCase_Table_Data.get("CostCategory"));
+		GuidewireAutomate("CostCategory", PD_CostCategory, "selectByVisibleText", lhm_TestCase_Table_Data.get("CostCategory"));
 
-		GuidewireAutomate("Recovery Category", CR_RecoveryCategory, "selectByVisibleText",
-				lhm_TestCase_Table_Data.get("RecoveryCategory"));
+		GuidewireAutomate("Recovery Category", CR_RecoveryCategory, "selectByVisibleText", lhm_TestCase_Table_Data.get("RecoveryCategory"));
 		GuidewireAutomate("Category", CR_Category, "selectByVisibleText", lhm_TestCase_Table_Data.get("Category"));
 		GuidewireAutomate("Amount", CR_Amount, "clearANDsendKeys", lhm_TestCase_Table_Data.get("Amount"));
 
