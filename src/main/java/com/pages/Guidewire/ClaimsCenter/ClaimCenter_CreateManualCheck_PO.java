36a1155d90d33e88e2913cbed28f8d1ade4f95d6
Akash Gowda <Akash Gowda@AL-01>
2021-08-25 17:16:33 +0530

    ReserveExecution and Payments Flow Completed

--- src/main/java/com/pages/Guidewire/ClaimsCenter/ClaimCenter_CreateManualCheck_PO.java
+++ src/main/java/com/pages/Guidewire/ClaimsCenter/ClaimCenter_CreateManualCheck_PO.java
@@ -17,8 +17,14 @@ public interface ClaimCenter_CreateManualCheck_PO {
 			.xpath("//div[contains(@id,'CheckPayeesScreen-ttlBar')]//div[@class='gw-TitleBar--title']");
 	final By Enterpaymentinformation_Header = By
 			.xpath("//div[contains(@id,'CheckPaymentsScreen-ttlBar')]//div[@class='gw-TitleBar--title']");
-	final By Setcheckinstructions_Header = By
-			.xpath("//div[contains(@id,'CheckInstructionsScreen-ttlBar')]//div[@class='gw-TitleBar--title']");
+	// Setcheckinstructions
+	final By Setcheckinstructions_Header = By.xpath(
+			"//div[contains(@class,'gw-isScreenTitle')]//div[@class='gw-TitleBar--title' and @role='heading' and contains(text(),'Step 3 of 3: Set check instructions')]");
+
+	final By SCI_PayToTheOrderOf = By.xpath(
+			"//div[contains(@id,'CheckWizardCheckSummaryInputSet-Check_PayTo')]//div[@class='gw-value-readonly-wrapper']");
+	final By SCI_MailingAddress = By.xpath(
+			"//div[contains(@id,'CheckWizardCheckSummaryInputSet-Check_MailTo_Input')]//div[@class='gw-value-readonly-wrapper']");
 	/*
 	 * CheckDetails
 	 */
@@ -30,6 +36,8 @@ public interface ClaimCenter_CreateManualCheck_PO {
 	 * Payment Details
 	 */
 	final By PD_ReserveLine = By.xpath("//select[contains(@name,'ReserveLineInputSet-ReserveLine')]");
+	final By PD_Coverage = By.xpath(
+			"//div[contains(@id,'ReserveLineInputSet-Coverage')]//div[@class='gw-RangeValue']/div[@class='gw-label']");
 	final By PD_PaymentType = By.xpath("//select[contains(@name,'Payment_PaymentType')]");
 	final By PD_CostType = By.xpath("//select[contains(@name,'ReserveLineInputSet-CostType')]");
 	final By PD_CostCategory = By.xpath("//select[contains(@name,'ReserveLineInputSet-CostCategory')]");
@@ -58,7 +66,34 @@ public interface ClaimCenter_CreateManualCheck_PO {
 
 	final By PrimaryPayee = By.xpath("//div[contains(@id,'NewCheckPayeeDV-1')]");
 
-	final By PD_Financial_Checks = By
-			.xpath("//div[contains(@class,'gw-isScreenTitle')]//div[@class='gw-TitleBar--title' and @role='heading']");
+	final By FinancialTranscations_Header = By.xpath(
+			"//div[contains(@class,'gw-isScreenTitle')]//div[@class='gw-TitleBar--title' and @role='heading' and contains(text(),'Financials: Transactions')]");
+
+	By FinancialsTransactions_CreateDate = By
+			.xpath("//div[contains(@id,'ClaimFinancialsTransactionsScreen-TransactionsLV-2-CreateDate')]");
+	By FinancialsTransactions_Amount = By
+			.xpath("//div[contains(@id,'ClaimFinancialsTransactionsScreen-TransactionsLV-2-Amount')]");
+	By FinancialsTransactions_Coverage = By
+			.xpath("//div[contains(@id,'ClaimFinancialsTransactionsScreen-TransactionsLV-2-CoverageType')]");
+	By FinancialsTransactions_CostType = By
+			.xpath("//div[contains(@id,'ClaimFinancialsTransactionsScreen-TransactionsLV-2-CostType')]");
+	By FinancialsTransactions_CostCategory = By
+			.xpath("//div[contains(@id,'ClaimFinancialsTransactionsScreen-TransactionsLV-2-CostCategory')]");
+	By FinancialsTransactions_Status = By
+			.xpath("//div[contains(@id,'ClaimFinancialsTransactionsScreen-TransactionsLV-2-Status')]");
+
+	// Financial Checks
+	final By FinancialChecks_Header = By.xpath(
+			"//div[contains(@class,'gw-isScreenTitle')]//div[@class='gw-TitleBar--title' and @role='heading' and contains(text(),'Financials: Checks')]");
+
+	final By FinancialChecks_PayTo_1 = By.xpath(
+			"//div[contains(@id,'ClaimFinancialsChecksScreen-ChecksLV-0-PayTo')]//div[@class='gw-value-readonly-wrapper']");
+	final By FinancialChecks_Status_1 = By
+			.xpath("//div[contains(@id,'ClaimFinancialsChecksScreen-ChecksLV-0-Status')]//div[@class='gw-label']");
+
+	final By FinancialChecks_PayTo_2 = By.xpath(
+			"//div[contains(@id,'ClaimFinancialsChecksScreen-ChecksLV-1-PayTo')]//div[@class='gw-value-readonly-wrapper']");
+	final By FinancialChecks_Status_2 = By
+			.xpath("//div[contains(@id,'ClaimFinancialsChecksScreen-ChecksLV-1-Status')]//div[@class='gw-label']");
 
 }
